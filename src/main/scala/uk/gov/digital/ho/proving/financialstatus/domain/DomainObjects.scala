93a6db7fdf8ba8319f66edc6599fa5c0466d2d50
Andy Birkett <andrew.birkett@digital.homeoffice.gov.uk>
2017-01-24 09:12:12 +0000

    PTTG-202 - Move consent service to common code

--- src/main/scala/uk/gov/digital/ho/proving/financialstatus/domain/DomainObjects.scala
+++ src/main/scala/uk/gov/digital/ho/proving/financialstatus/domain/DomainObjects.scala
@@ -2,9 +2,6 @@ package uk.gov.digital.ho.proving.financialstatus.domain
 
 import java.time.LocalDate
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonInclude.Include
-
 case class Account(sortCode: String, accountNumber: String)
 
 case class AccountDailyBalances(accountHolderName: String, balances: Seq[AccountDailyBalance])
